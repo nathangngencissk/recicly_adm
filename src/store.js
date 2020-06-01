ec22a265d5e78a5eb1fd3650f94aec8615d593cd
Nathan George Nascimento Gencissk <nathangngencissk@gmail.com>
2020-05-31 23:54:14 -0300

    Corret linting errors, delete template from project

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
@@ -10,14 +10,14 @@ export default new Vuex.Store({
     drawer: null,
   },
   mutations: {
-    SET_BAR_IMAGE (state, payload) {
-      state.barImage = payload
+    SET_BAR_IMAGE(state, payload) {
+      state.barImage = payload;
     },
-    SET_DRAWER (state, payload) {
-      state.drawer = payload
+    SET_DRAWER(state, payload) {
+      state.drawer = payload;
     },
   },
   actions: {
 
   },
-})
+});
